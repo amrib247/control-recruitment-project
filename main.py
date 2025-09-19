57f3683786bd90e13e60b3397bbbbf6a15a66b79
Amr Ibrahim <amr.ali.ibrahim247@gmail.com>
2025-09-18 20:29:03 -0700

    Generalized get_nearest_cones to be able to filter out cones, and get the nearest k cones to the vehicle

--- main.py
+++ main.py
@@ -13,40 +13,47 @@ def controller(x):
     Returns:
         ndarray: numpy array of shape (2,) containing [fwd acceleration, steering rate]
     """
-    offset1 = 5
-    offset2 = 5
-
-    cone = closest_cone(x)
-    point = cone + np.array([offset1, offset2])
-    angle_to_cone = math.atan((point[0] - x[0])/(point[1] - x[1]))
-    return np.array([11.99, -math.tanh(x[2] - angle_to_cone)])
+    # Gets nearest 2 cones to the right of the middle of the vehicle
+    cones = get_closest_cones(x, 2, cond = lambda cone: cone[1] - x[1] < math.tan(x[2]) * (cone[0] - x[0])) 
+    point = np.array([cones[0][0] - cones[1][0], cones[0][1] - cones[1][1]]) # Midpoint of 2 cones
+    
+    
+    #print(cones)
+    #angle_to_cone = math.atan((point[0] - x[0])/(point[1] - x[1]))
+    return np.array([11.99, (x[2] - angle_to_cone) * -100])
 
 def closest_cone(x):
+        """Returns the single closest cone"""
+
         return get_closest_cones(x, 1)[0]
 
 def get_distance(x, cone):
+    """Gives the distance from the vehicle to a given cone ([x, y])"""
+
     return (x[0] - cone[0]) ** 2 + (x[1] - cone[1]) ** 2
 
 def get_distances(x, arr):
+    """Returns an array of distances from the car to an array of cones"""
+
     ret = np.full(len(arr), 0.)
     for i in range(len(arr)):
         ret[i] = get_distance(x, arr[i])
     return ret
 
-def get_closest_cones(x, k):
+def get_closest_cones(x, k, cond = lambda cone: True):
+    """Gets the k closest cones to the car, with the optional paramater cond. Cond takes in a
+    set of cone coordinates and returns a boolean whether to check the cone or not. This can be
+    used to exclude cones"""
 
     cones = sim.cones
-    #print(cones)
     closest = np.full((k, 2), np.full(2, math.inf))
     for cone in cones: 
-        #print(cone[0])
-        if get_distance(x, cone) < min(get_distances(x, closest)):
+        if cond(cone) and get_distance(x, cone) < max(get_distances(x, closest)):
             max_idx = 0
             for i in range(len(closest)):
                 if get_distance(x, closest[max_idx]) < get_distance(x, closest[i]):
-                    max_idx = 1
-            closest[i] = cone
-
+                    max_idx = i
+            closest[max_idx] = cone
     return closest
     
 sim.set_controller(controller)
